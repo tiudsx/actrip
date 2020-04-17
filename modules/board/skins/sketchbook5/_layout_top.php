ac31d123e8d35b55cac67ee032dbbbeb9422ed94
이승철_jt <lud1@naver.com>
2020-04-17 19:17:14 +0900

    네비게이션

--- modules/board/skins/sketchbook5/_layout_top.php
+++ modules/board/skins/sketchbook5/_layout_top.php
@@ -1,9 +1,9 @@
 <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
     
 <link rel="stylesheet" type="text/css" href="/act/css/default.css">
-<link rel="stylesheet" href="/act/css/swiper.min.css">
+<link rel="stylesheet" type="text/css" href="/act/css/swiper.min.css">
 <link rel="stylesheet" type="text/css" href="/act/css/gnbstyle.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css">
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css">
 <link rel="stylesheet" type="text/css" href="/act/css/swiper.min.css">
 
 <div id="wrap">
@@ -22,16 +22,16 @@
 							<!--@else-->
 							<li><a href="/index.php?mid=main&act=dispMemberLoginForm"><img src="/act/images/icon/user-solidg.svg" alt=""><p>로그인</p></a></li>
 							<!--@end-->	
-							<li><a href="#"><img src="/act/images/icon/shoppingg.svg" alt=""><p>장바구니</p></a></li>
-							<li><a href="#"><img src="/act/images/icon/checkg.svg" alt=""><p>예약조회</p></a></li>
+							<!-- <li><a href="#"><img src="/act/images/icon/shoppingg.svg" alt=""><p>장바구니</p></a></li> -->
+							<li><a href="/ordersearch"><img src="/act/images/icon/checkg.svg" alt=""><p>예약조회</p></a></li>
 						</ul>
 						<ul class="menu02">
 							<li><a href="/surf"><img src="/act/images/icon/isurf.png" alt=""></a></li>
 							<li><a href="/surfbus"><img src="/act/images/icon/ibus.png" alt=""></a></li>
-							<li><a href="#"><img src="/act/images/icon/ibbq.png" alt=""></a></li>
-							<li><a href="#"><img src="/act/images/icon/itent.png" alt=""></a></li>
-							<li><a href="#"><img src="/act/images/icon/ibed.png" alt=""></a></li>
-							<li><a href="#"><img src="/act/images/icon/ifood.png" alt=""></a></li>
+							<li><a href="/bbq"><img src="/act/images/icon/ibbq.png" alt=""></a></li>
+							<li><a href="/camp"><img src="/act/images/icon/itent.png" alt=""></a></li>
+							<li><a href="/staylist"><img src="/act/images/icon/ibed.png" alt=""></a></li>
+							<li><a href="/eatlist"><img src="/act/images/icon/ifood.png" alt=""></a></li>
 						</ul>
 						<ul class="menu03">
 							<li><a href="https://pf.kakao.com/_HxmtMxl" target="_blank"><img src="/act/images/icon/cscenter.svg" alt="">고객센터</a></li>
@@ -57,7 +57,7 @@
 			<h1 id="logo">
 				<a href="/"><img src="/act/images/logo140.jpg" alt="액트립 로고"></a>
 			</h1>
-			<div class="shopIcon"><a href="#"><img src="/act/images/icon/shop.svg" alt="장바구니"></a></div>
+			<!-- <div class="shopIcon"><a href="#"><img src="/act/images/icon/shop.svg" alt="장바구니"></a></div> -->
 		</div>
 	</header>
 
